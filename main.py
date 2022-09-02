14ef8dbec5a97b48104845990534c8a6eb3c553b
Vinay Kumar <55936553+Linuxinet@users.noreply.github.com>
2022-09-02 08:09:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def newbt(client,callback_query):
 
 @app.on_message(filters.command(["about"]))
 def about(client, message):
-    client.send_message(chat_id=message.chat.id, reply_to_message_id=message.Message.id,
+    client.send_message(chat_id=message.chat.id, reply_to_message_id=message.id,
                         text=f"<b>`⭕ Creator ⭕` : @linuxinet\n\n`⭕ Language ⭕ :` [Python3](https://python.org)\n\n`⭕ Library ⭕ :` [Pyrogram](https://docs.pyrogram.org/)\n\n`⭕ Server ⭕ :` [Heroku Professional](https://herokuapp.com/)</b>",
                         disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("Support Group", url="t.me/HxSupport")]]))
@@ -59,7 +59,7 @@ def echo(client, message):
         firebase.put('/stats', 'total_searches', data)
     global pq
     pq = ""
-    pro = client.send_message(chat_id=message.chat.id, text="Searching...", reply_to_message_id=message.Message.id)
+    pro = client.send_message(chat_id=message.chat.id, text="Searching...", reply_to_message_id=message.id)
     r_num = message.text
     num = r_num.replace("+91", "").replace(" ", "")
     frbseyename = ""
